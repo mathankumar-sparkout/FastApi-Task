e0f1672098ed03c5488fb60c42004c43338087b0
mathankumar-sparkout <mathankumar@sparkouttech.com>
2024-05-27 16:01:15 +0530

    add file

--- main.py
+++ main.py
@@ -16,4 +16,4 @@ app.include_router(user.router)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.2", port=7000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
